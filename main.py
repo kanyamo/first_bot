9b8524b19f11795e68042113f3560e3860e3bb65
kanyamo <kanajir@yahoo.co.jp>
2023-02-05 19:27:40 +0900

    実際にプロフィールを取得するようにする

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 import os
 from dotenv import load_dotenv
+import requests
+import json
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -15,12 +17,68 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    # ボット自身のメッセージに反応しないようにする
     if message.author == client.user:
         return
 
-    if message.content.startswith('\hello_world'):
-        await message.channel.send('Hello, World!')
+    content = message.content
+
+    if content.startswith('prof'):
+
+        subcommands = content.strip().split()[1:]
+
+        if len(subcommands) == 0:
+            await describe(message)
+            return
+
+        if subcommands[0] == "help":
+            await show_help(message)
+            return
+
+        if subcommands[0] == "show":
+            try:
+                if len(subcommands) == 1:
+                    result = (
+                        "第二引数にユーザーidを指定してください。\n"
+                        "例： `prof show 286685511145242625`"
+                    )
+                    await message.channel.send(result)
+                    return
+                else:
+                    user_id = subcommands[1]
+                    url = f"https://api.pjsekai.moe/api/user/{user_id}/profile"
+                    response = requests.get(url)
+                    data = json.loads(response.text)
+                    game_data = data['user']['userGamedata']
+                    result = (
+                        f"{game_data['name']} のプロフィール\n"
+                        f"ランク：{game_data['rank']}"
+                    )
+                    await message.channel.send(result)
+            except Exception as e:
+                await message.channel.send(e)
+            return
+
+
+async def describe(message):
+    description = (
+        "Hello, World!\n"
+        "`prof help`  コマンドの使い方を表示します。"
+    )
+    await message.channel.send(description)
+
+
+async def show_help(message):
+    description = (
+        "サブコマンド一覧\n"
+        "`help`  コマンドの使い方を表示します。\n"
+        "`show`  ユーザーのプロフィールを表示します。第二引数にユーザーidを入力します。"
+    )
+
+    await message.channel.send(description)
 
 load_dotenv()
 
-client.run(os.getenv('TOKEN'))
+TOKEN = os.getenv('TOKEN')
+
+client.run(TOKEN)
